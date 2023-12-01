b52bdf27969ea03fe1bd9af1b8f52c587c153ad9
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2023-12-01 09:52:02 -0700

    Adding template to repo

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <input filename>" << std::endl;
+        return 0;
+    }
+
+    std::ifstream fin(argv[1]);
+    if (fin.fail())
+    {
+        std::cout << "Could not open file " << argv[1] << std::endl;
+        return 0;
+    }
+
+    while(!fin.eof())
+    {
+
+    }
+    fin.close();
+
+    return 0;
+}
