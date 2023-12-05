2b243987e0f5d574a53ba9fc865f672fcc0858f6
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2023-12-05 10:34:47 -0700

    added string to int64_t method

--- helpers/helpers.cpp
+++ helpers/helpers.cpp
@@ -1,7 +1,9 @@
 #include "helpers.h"
 
-#include <vector>
+#include <cctype>
+#include <cstdint>
 #include <string>
+#include <vector>
 
 std::vector<std::string> helpers::splitString(const std::string& string, const std::vector<std::string>& delimiters)
 {
@@ -26,4 +28,24 @@ std::vector<std::string> helpers::splitString(const std::string& string, const s
         result.push_back(string.substr(lastSplit, c - lastSplit));
 
     return result;
+};
+
+
+
+int64_t helpers::stoi64(std::string string)
+{
+    if(string.length() < 1)
+        return 0;
+
+    bool negative = string[0] == '-';
+    int64_t number = 0;
+
+    for(int i = (negative ? 1 : 0); i < string.length() && isdigit(string[i]); i++)
+    {
+        number *= 10;
+        number += string[i] - '0';
+    }
+
+    number *= (negative ? -1 : 1);
+    return number;
 };
--- helpers/helpers.h
+++ helpers/helpers.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
 namespace helpers {
 
 std::vector<std::string> splitString(const std::string& string, const std::vector<std::string>& delimiters);
+int64_t stoi64(std::string string);
 
 } // helpers
